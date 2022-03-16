f0a8e625d803072dc20fbc25e88d85359f085c2c
Max Hwang <maxhwang15@gmail.com>
2022-03-17 03:12:06 +0900

    2022.03.11 first HackerRank

---
+++ src/com/hackerRank/main/run.java
@@ -0,0 +1,9 @@
+package com.hackerRank.main;
+
+import com.hackerRank.practice.LoopII;
+
+public class run {
+    public static void main(String[] args){
+        new LoopII().method1();
+    }
+}
--- src/com/hackerRank/practice/LoopII.java
+++ src/com/hackerRank/practice/LoopII.java
@@ -1,4 +1,33 @@
 package com.hackerRank.practice;
 
+import java.util.Scanner;
+
 public class LoopII {
+    public void method1(){
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        for(int i = 0; i < t; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int n = sc.nextInt();
+            int sum = a;
+
+            for(int j = 0; j < n; j++){
+                int num = (int)Math.pow(2, j) * b;
+                sum = sum + num;
+                System.out.printf("%s ", sum); // %s 안에 sum 을 집어넣기
+                // printf 서식이 있는 출력
+                // %d : 정수
+                // %f : 실수
+                // %c : 문자
+                // %s : 문자열
+            }
+            if(i < t - 1){
+                System.out.print("\n");
+            }
+        }
+        sc.close();
+    }
 }
