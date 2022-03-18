c3b76e28bc4407c4df64cea63b93fe1be7c269ba
Max Hwang <maxhwang15@gmail.com>
2022-03-19 01:45:15 +0900

    2022.03.19 DataType(byte, short, int, long)

--- src/com/hackerRank/main/run.java
+++ src/com/hackerRank/main/run.java
@@ -1,9 +1,14 @@
 package com.hackerRank.main;
 
+import com.hackerRank.practice.DataType;
 import com.hackerRank.practice.LoopII;
+
+import javax.xml.crypto.Data;
 
 public class run {
     public static void main(String[] args){
-        new LoopII().method1();
+        // new LoopII().method1();
+        new DataType().method1();
+
     }
 }
